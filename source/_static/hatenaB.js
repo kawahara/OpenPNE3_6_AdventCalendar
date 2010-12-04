01c1c88072e8e0d759bfafa7a1af766e75195963
Shogo Kawahara <kawahara@bucyou.net>
2010-12-04 16:50:27 +0900

    はてなブックマーク用スクリプト修正

--- source/_static/hatenaB.js
+++ source/_static/hatenaB.js
@@ -5,17 +5,23 @@ $('document').ready(function(){
     var f = path.substring(path.lastIndexOf ('/',path.length) +1, path.length);
     if (f && 0 === f.indexOf('index.'))
     {
-      path = path.substring(0, path.lastIndexOf('/'));
+      path = path.substring(0, path.lastIndexOf('/')) + '/';
     }
 
     var url = encodeURI(l.protocol + '//' + l.host + path);
 
-    var img = $('<img />').attr({
+    var img1 = $('<img />').attr({
+      'src'   : 'http://b.hatena.ne.jp/images/append.gif',
+      'alt'   : 'はてなブックマーク',
+      'title' : 'はてなブックマーク'
+    });
+    var img2 = $('<img />').attr({
       'src'   : 'http://b.hatena.ne.jp/entry/image/' + url,
       'alt'   : 'はてなブックマーク',
       'title' : 'はてなブックマーク'
     });
-    var a = $('<a />').append(img).attr({
+
+    var a = $('<a />').append(img1).append(img2).attr({
       'href'  : 'http://b.hatena.ne.jp/entry/' + url
     });
     var span = $('<span />').append(a).css({'font-size':"0.5em"}).appendTo(this);
